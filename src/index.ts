8d4d910f660fb90132e3caa70cc1b26bf6db8dbd
ong-ar <reclusis@gmail.com>
2022-07-06 19:29:01 +0900

    add cosmos

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
+import * as cosmosFunctions from './cosmos';
 import { InstallError } from './error';
-import { addChain, getAccount, getSupportedChains, offAccountChanged, onAccountChanged, requestAccount, signAmino, signDirect } from './tendermint';
+import * as tendermintFunctions from './tendermint';
 
 export { InstallError };
-
-export type Tendermint = {
-  addChain: typeof addChain;
-  getAccount: typeof getAccount;
-  requestAccount: typeof requestAccount;
-  signAmino: typeof signAmino;
-  signDirect: typeof signDirect;
-  getSupportedChains: typeof getSupportedChains;
-  onAccountChanged: typeof onAccountChanged;
-  offAccountChanged: typeof offAccountChanged;
-};
 
 export function isInstalled() {
   return !!window.cosmostation;
 }
 
-export function tendermint(): Promise<Tendermint> {
+export function cosmos(): Promise<typeof cosmosFunctions> {
   return new Promise((resolve, reject) => {
     const interval = setInterval(() => {
       if (isInstalled()) {
         clearInterval(interval);
-        resolve({ addChain, requestAccount, signAmino, signDirect, getSupportedChains, onAccountChanged, offAccountChanged, getAccount });
+        resolve(cosmosFunctions);
+      }
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      reject(new InstallError());
+    }, 500);
+  });
+}
+
+// legacy
+export function tendermint(): Promise<typeof tendermintFunctions> {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (isInstalled()) {
+        clearInterval(interval);
+        resolve(tendermintFunctions);
       }
     }, 100);
 
