8e5d56ef1d9451d019517fbf180ff0e8927c1f62
ong-ar <reclusis@gmail.com>
2022-07-06 20:48:17 +0900

    change tendermint -> cosmos

--- src/cosmos.ts
+++ src/cosmos.ts
@@ -11,39 +11,39 @@ import type {
 } from './types/message';
 
 export function getSupportedChains() {
-  return window.cosmostation.tendermint.request({ method: 'cos_supportedChainNames' }) as Promise<SupportedChainNamesResponse>;
+  return window.cosmostation.cosmos.request({ method: 'cos_supportedChainNames' }) as Promise<SupportedChainNamesResponse>;
 }
 
 export function getAccount(chainName: string) {
-  return window.cosmostation.tendermint.request({ method: 'cos_account', params: { chainName } }) as Promise<AccountResponse>;
+  return window.cosmostation.cosmos.request({ method: 'cos_account', params: { chainName } }) as Promise<AccountResponse>;
 }
 
 export function requestAccount(chainName: string) {
-  return window.cosmostation.tendermint.request({ method: 'cos_requestAccount', params: { chainName } }) as Promise<RequestAccountResponse>;
+  return window.cosmostation.cosmos.request({ method: 'cos_requestAccount', params: { chainName } }) as Promise<RequestAccountResponse>;
 }
 
 export function addChain(chain: AddChainParams) {
-  return window.cosmostation.tendermint.request({ method: 'cos_addChain', params: { ...chain } }) as Promise<boolean>;
+  return window.cosmostation.cosmos.request({ method: 'cos_addChain', params: { ...chain } }) as Promise<boolean>;
 }
 
 export function signAmino(chainName: string, doc: SignAminoDoc, options?: SignOptions) {
-  return window.cosmostation.tendermint.request({
+  return window.cosmostation.cosmos.request({
     method: 'cos_signAmino',
     params: { chainName, doc, isEditMemo: !!options?.memo, isEditFee: !!options?.fee, gasRate: options?.gasRate },
   }) as Promise<SignAminoResponse>;
 }
 
 export function signDirect(chainName: string, doc: SignDirectDoc, options?: SignOptions) {
-  return window.cosmostation.tendermint.request({
+  return window.cosmostation.cosmos.request({
     method: 'cos_signDirect',
     params: { chainName, doc, isEditMemo: !!options?.memo, isEditFee: !!options?.fee, gasRate: options?.gasRate },
   }) as Promise<SignDirectResponse>;
 }
 
 export function onAccountChanged(handler: () => void) {
-  return window.cosmostation.tendermint.on('accountChanged', handler);
+  return window.cosmostation.cosmos.on('accountChanged', handler);
 }
 
 export function offAccountChanged(event: unknown) {
-  window.cosmostation.tendermint.off(event);
+  window.cosmostation.cosmos.off(event);
 }
--- src/types/d.ts/window.d.ts
+++ src/types/d.ts/window.d.ts
@@ -6,6 +6,11 @@ interface Window {
       off: (handler: unknown) => void;
       sendAsync: () => null;
     };
+    cosmos: {
+      request: (message: { method: string; params?: unknown }) => Promise<T>;
+      on: (eventName: string, eventHandler: (event?: unknown) => void) => unknown;
+      off: (handler: unknown) => void;
+    };
     tendermint: {
       request: (message: { method: string; params?: unknown }) => Promise<T>;
       on: (eventName: string, eventHandler: (event?: unknown) => void) => unknown;
